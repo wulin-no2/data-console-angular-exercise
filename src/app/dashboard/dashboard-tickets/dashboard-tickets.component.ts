ded835a9044ad2222f8c2c133a12258dc8923ce6
Lina <wulinthu@gmail.com>
2026-01-23 16:21:44 +0000

    Section6-Adding new ticket component

--- src/app/dashboard/dashboard-tickets/dashboard-tickets.component.ts
+++ src/app/dashboard/dashboard-tickets/dashboard-tickets.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { DashboardItemComponent } from "../dashboard-item/dashboard-item.component";
+import { NewTicketComponent } from "../tickets/new-ticket/new-ticket.component";
 
 @Component({
   selector: 'app-dashboard-tickets',
   standalone: true,
   templateUrl: './dashboard-tickets.component.html',
-  styleUrl: './dashboard-tickets.component.css'
+  styleUrl: './dashboard-tickets.component.css',
+  imports: [NewTicketComponent]
 })
 export class DashboardTicketsComponent {
 
